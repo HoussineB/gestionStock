f93a321a30783f8221d2508b9c18d0b4fe4d6c6d
Houssine Barhoumi <h.Barhoumi@enedix.fr>
2024-02-17 16:27:10 +0100

    PSV-5 modidfier sous titre 3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Gestion de stock</h1>
         <h2>Groupe TTC 2024</h2>
-        <h3>Bonne Chance</h3>
+        <h3>Bonne Chance Mes amis</h3>
       </header>
     </div>
   );
