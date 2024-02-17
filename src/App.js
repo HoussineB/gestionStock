f917010cf6070cefbf225521e8f51b89b936ced7
Userilhem <ilhemkahlaoui2020@gmail.com>
2024-02-17 17:57:16 +0100

    PSV-3 stock sortie

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <h2>Groupe TTC 2024</h2>
         <h3>Bonne Chance Mes amis</h3>
         <h4>Gerer Stock</h4>
+        <h1>stock sortie</h1>
       </header>
     </div>
   );
