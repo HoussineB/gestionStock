43044a75de0a4ac10306c73376f33135cd4cc4bf
Houssine Barhoumi <h.Barhoumi@enedix.fr>
2024-02-17 16:25:30 +0100

    ajouter sous titre 3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Gestion de stock</h1>
         <h2>Groupe TTC 2024</h2>
+        <h3>Bonne Chance</h3>
       </header>
     </div>
   );
